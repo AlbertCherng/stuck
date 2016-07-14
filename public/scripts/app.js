8e863638e38a67f9523e8df0c0ef35c25f3ab52c
vagrant <albert.cherng@gmail.com>
2016-07-14 02:59:16 +0000

    Made changes to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,83 @@ $(() => {
     }
   });;
 });
+
+
+function loadPoll() {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/tweets",
+    success: function(data) {
+      renderPoll(data)
+    }
+  });
+}
+
+
+function loadChoices() {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/tweets",
+    success: function(data) {
+      renderChoices(data)
+    }
+  });
+}
+
+
+
+function renderPoll(tweets) {
+  var $tweets = $('.posted-tweets').empty();
+  for (index in tweets) {
+    $tweets.append(createTweetElement(tweets[index]));
+  };
+}
+
+function renderChoices(tweets) {
+  var $tweets = $('.posted-tweets').empty();
+  for (index in tweets) {
+    $tweets.append(createTweetElement(tweets[index]));
+  };
+}
+
+function createTweetElement(tweet) {
+  var html = `<article>
+                <header class="tweet-header">
+                  <img src="${tweet.user.avatars.small}" class="user-image">
+                  <p class="user-name">${tweet.user.name}</p>
+                  <p class="user-handle">${tweet.user.handle}</p>
+                </header>
+                <section class="tweet-body">
+                  ${tweet.content.text}
+                </section>
+                <footer class="tweet-footer">
+                  <p class="post-date"> ${tweet.created_at} </p>
+                  <span class="icons" id="iconspan">
+                    <i class="fa fa-font-awesome" aria-hidden="true"></i>
+                    <i class="fa fa-retweet" aria-hidden="true"></i>
+                    <i class="fa fa-heart" aria-hidden="true"></i>
+                  </span>
+                </footer>
+              </article>`
+  return $(html);
+}
+
+$(function() {
+  $("#tweetForm").submit(function(event) {
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/tweets/",
+      data: $(this).serialize(),
+      success: function(res) {
+        console.log(res);
+        $("#tweetForm")[0].reset();
+        $(".counter").text(140);
+        loadTweets();
+      }
+    });
+  };
+  });
+  loadPoll();
+  loadChoices();
+});
