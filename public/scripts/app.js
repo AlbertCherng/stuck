ea368d53a59a66927fa2a45a78dbca2519b1d0de
vagrant <albert.cherng@gmail.com>
2016-07-15 03:00:16 +0000

    Add indexing to additional choices and participants

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,8 +4,6 @@ $(function() {
   });
 
   $( ".poll" ).on("keyup", function() {
-    // var id = $(this).data-poll-id
-    console.log("Working!")
     $.ajax({
       method: "PUT",
       url: "./",
@@ -13,46 +11,47 @@ $(function() {
     });
   })
 })
+
 function createParticipant() {
-  let index = 0; // TODO
-  return $(`<div class="row">
-  <div class="col-md-4 col-md-offset-3">
-    <div class="form-group">
-      <input type="email" class="form-control" name="poll[participants][${index}][email]" placeholder="enter email" value="">
-    </div>
-  </div>
-</div>`);
+  let index =  $(".poll .participant").length;
+  return $(`<div class="participant">
+              <div class="row">
+                <div class="col-md-4 col-md-offset-3">
+                  <div class="form-group">
+                    <input type="email" class="form-control" name="poll[participants][${index}][email]" placeholder="enter email" value="">
+                  </div>
+                </div>
+              </div>
+            </div>`);
 }
 
 function createChoice() {
-  let index = 0; // TODO
-  return $(`<div class="row">
-    <div class="col-md-4 col-md-offset-3">
-      <div class="form-group">
-        <input type="text" class="form-control" name="poll[choices][${index}][title]" placeholder="Choice" value="">
-      </div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col-md-4 col-md-offset-3">
-    <div class="form-group">
-      <textarea class="form-control" rows="2" name="poll[choices][${index}][description]" placeholder="Uh huh uh huh, makes sense."></textarea>
-    </div>
-  </div>
-</div>`);
+  let index = $(".poll .choice").length;
+  return $(`<div class="choice">
+              <div class="row">
+                <div class="col-md-4 col-md-offset-3">
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="poll[choices][${index}][title]" placeholder="Choice" value="">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 col-md-offset-3">
+                  <div class="form-group">
+                    <textarea class="form-control" rows="2" name="poll[choices][${index}][description]" placeholder="Uh huh uh huh, makes sense."></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>`);
 }
 
 $(document).ready(function() {
-
   $('.addChoice').click(function(event) {
     createChoice().appendTo('.choices');
   });
-
  $('.addParticipant').click(function(event) {
    createParticipant().appendTo('.participants');
 });
-
 });
 
 
