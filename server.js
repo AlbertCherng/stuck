2be3bf027c1b2b59671c7d5a81c04e62b0f5c6f0
mariaxtina <mchristinahess@gmail.com>
2016-07-17 20:52:06 -0700

    merge issues

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 require('dotenv').config();
 
 const PORT        = process.env.PORT || 8080;
@@ -7,7 +9,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
 const app         = express();
-//const bootstrap   = require('bootstrap');
 
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
@@ -48,175 +49,64 @@ app.get('/a/:admin_digest', (req, res) => {
     }
   });
 
-});
+})
 
 app.post("/polls", (req, res) => {
-  knex('polls').insert({}).returning('id').then(function(results) {
+  // create a poll
+  knex('polls').insert({}).returning("id").then(function(results) {
     let id = results[0];
     res.redirect(`/polls/${id}/edit`);
   });
+
 });
 
 app.get("/polls/:id/edit", (req, res) => {
   // fetch poll from db
   knex('polls').select('*').where({id: req.params.id})
-  .then(function(polls) {
-    knex('choices').select('*').where({poll_id: req.params.id})
-    .then(function(choices) {
-      knex('choices').select('*').where({poll_id: req.params.id})
-    .then(function(participants) {
-      let form_choices = choices;
-      let form_participants = participants;
-
-      if (form_choices.length == 0) {
-        form_choices = [{},{},{}];
-      }
-      if (participants.length == 0) {
-        form_participants = [{},{},{}];
-      }
-      res.render("poll_edit", {poll: polls[0],
-                               choices: form_choices,
-                               participants: form_participants});
-     });
-    });
+  .then(function(results) {
+    res.render("poll_edit", {poll: results[0]});
   });
 
-  });
+});
 
+app.put("/polls/:id" (req, res) => {
+  // save updates
+  knex('polls').insert({id: req.params.id});
+});
 
-app.put("/polls/:id", (req, res) => {
-  let poll_id = req.params.id;
-  console.log(req.body.poll)
-  knex('polls').where({id: poll_id}).update({title: req.body.poll.title,
-                                             description: req.body.poll.description}).then();
-  knex('choices').where({poll_id: poll_id}).delete().then();
-  knex('participants').where({poll_id: poll_id}).delete().then();
-
-
-  for(choice of req.body.poll.choices) {
-    knex('choices').insert({title: choice.title,
-                             description: choice.description,
-                             poll_id: poll_id}).then();
-  }
-
-  for(participant of req.body.poll.participants) {
-    knex('participants').insert({email: participant.email,
-                                poll_id: poll_id}).then();
-  }
-  res.json({status: "OK"})
+app.get("/polls/:id", (req, res) => {
+  res.render("poll_share");
 });
 
 app.get("/polls/:id/results", (req, res) => {
-
-  knex.raw(`SELECT polls.id, rankings.choice_id, rankings.ranking, rankings.participant_id, choices.title
-            FROM polls
-            JOIN choices ON choices.poll_id = polls.id
-            JOIN rankings ON (rankings.choice_id = choices.id)
-            WHERE polls.id = choices.poll_id`).then(function(results){
-              var checkArr = [];
-              var finalObj = {};
-              var answer = {};
-              var titles = [];
-
-              console.log(results.rows);
-              for(item of results.rows){
-                var finalArr = [];
-                if(checkArr.indexOf(item.choice_id) === -1){
-                  titles.push(item.title)
-
-                  checkArr.push(item.choice_id);
-                  finalArr.push(item.ranking);
-
-                  finalObj[item.choice_id] = finalArr;
-                } else {
-                    finalObj[item.choice_id].push(item.ranking);
-                }
-                finalObj[item.choice_id].reduce(function(a, b){
-                  return a + b;
-                },0);
-              }
-              for (choice in finalObj) {
-                var finalScoreArr = finalObj[choice]
-                // console.log(finalScoreArr);
-
-                answer[choice] = finalScoreArr.reduce((a, b) => a + b, 0);
-                console.log(finalScoreArr);
-              }
-
-              console.log(titles);
-              console.log(answer);
-
-              answers = Object.keys(answer).sort(function(a,b){return answer[a]-answer[b]})
-              res.render("poll_results", {id: req.params.id,
-                                          answer: answers,
-                                          results: results.rows,
-                                          titles: titles
-                                         });
-            })
+  res.render("poll_results");
 });
 
 app.get("/p/:participant_digest", (req, res) => {
   // check if digest correct // find which poll it correspdonds to
   // create a participant // set a cookie
     knex('polls').where({participant_digest: req.params.participant_digest})
-    .select('id').then(function(results) {
-
-      if(results.length == 0) {
-        throw error;
-      } else {
-        let poll_id = results[0].id;
-        knex('participants').insert({poll_id: poll_id}).returning("id").then(function(results) {
-          let participant_id = results[0];
-          res.cookie("participant_id", participant_id);
-          res.redirect(`/rank/${poll_id}`);
-        });
-      }
-  });
-});
-
-app.get("/rank/:poll_id", (req, res) => {
-  let poll_id = req.params.poll_id;
-  knex('polls').select('title', 'description').where({id: poll_id})
-  .then(function(polls) {
-     knex('choices').select('id', 'title', 'description').where({poll_id: poll_id})
-     .then(function(choices) {
-        res.render("poll_taking", {poll: polls[0],
-                                  choices: choices});
-     })
-  })
-});
-
-/*
-  { rankings: [ id1 id2 id3 id4 id5 ]}
-*/
-
-app.post("/rank/:poll_id", (req, res) => {
-
-  console.log(req.body.ranking)
-  var rankingsCreated = 0;
-
-  var onAllComplete = function () {
-    if (rankingsCreated === req.body.ranking.length){
-     res.redirect(`/rank/${req.params.poll_id}/success`);
+    .select('participant_id, id').then(function(results) {
+    if(results.length == 0){
+      throw error;
+    } else {
+      const participant_id = results[0].participant_id;
+      const id = results[0].id;
+      req.cookies("participant_id", participant_id);
+      res.redirect(`/rank/${participant_id}`);
     }
-  }
-
-  req.body.ranking.forEach(function(choice_id, index){
-
-     knex('rankings').insert({participant_id: req.cookies.participant_id,
-                           choice_id: choice_id,
-                           ranking: index
-                         }).then(function(result) {
-                          rankingsCreated += 1;
-                          onAllComplete();
-  });
-
-  });
-
 
 });
 
-app.get("/rank/:participant_id/success", (req, res) => {
+app.get("/rank/:participant_id", (req, res) => {
+  res.render("poll_taking");
+});
+
+app.post("/rank/:participant_id", (req, res) => {
+  res.redirect("/rank/:participant_id/success");
+});
+
+app.post("/rank/:participant_id/success", (req, res) => {
   res.render("poll_success");
 });
 
